28496fa9735e9e27a298b586a718d80148a81d02
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-25 07:16:24 +0900

    Room DB에 대해서 주석 추가

--- app/src/main/java/com/example/todolist/dao/TodoDao.kt
+++ app/src/main/java/com/example/todolist/dao/TodoDao.kt
@@ -7,6 +7,7 @@ import com.example.todolist.dto.Todo
 @Dao
 interface TodoDao {
 
+    // onConflict - 충돌처리방식 / OnConflictStrategy.REPLACE - 충돌이 발생할 경우 덮어쓰기
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(dto: Todo)
 
@@ -27,4 +28,13 @@ interface TodoDao {
 *   insert, query, update, delete는 Room 어노테이션을 사용해 구성
 *   모든 항목을 불러오는 list 함수의 경우 LiveData를 사용해, 추가, 수정, 삭제에 의해
 *   변화하는 값에 대해 즉시 반영이 가능하도록 한다.
+*
+*   @Query를 사용해서 DB를 조회
+*
+*   insert의 충돌처리방식에는 5가지가 있다.
+*   OnConflictStrategy.ABORT - 충돌이 발생ㄴ할 경우 처리 중단
+*   OnConflictStrategy.FAIL - 충돌이 발생할 경우 실패처리
+*   OnConflictStrategy.IGNORE - 충돌이 발생할 경우 무시
+*   OnConflictStrategy.REPLACE - 충돌이 발생할 경우 덮어쓰기
+*   OnConflictStrategy.ROLLBACK - 충돌이 발생할 경우 이전으로 되돌리기
 */
--- app/src/main/java/com/example/todolist/database/TodoDatabase.kt
+++ app/src/main/java/com/example/todolist/database/TodoDatabase.kt
@@ -12,4 +12,9 @@ abstract class TodoDatabase: RoomDatabase() {
 
 /*
 *    entity는 투두 클래스로, RoomDatabase 라이브러리를 사용해 생성
+*
+*   entities - 해당 DB에 어떤 테이블들이 있는지 명시
+*   version - Schema가 바뀔 때 이 version도 바뀌어야 한다.
+*   exportSchema - Room의 Schema구조를 폴더로 Export 할 수 있다.
+*                   데이터베이스의 버전 히스토리를 기록할 수 있다는 점에서 true로 설정하는 것이 좋다.
 */
